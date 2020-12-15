67dd437f8938df4dc989175b8887512bd0175352
sumonsheikh <sm.sumon117@gmail.com>
2020-12-15 18:50:41 +0600

    create fetchJobs function for fetch the data from api

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import './App.css';
 const url ='https://course-api.netlify.app/api/react-tabs-project';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs]       = useState([]);
+  const [value, setValue]     = useState(0);
+
+  const fetchJobs = async () =>{
+    const respons = await fetch(url);
+    const newJobs = await respons.json();
+    setJobs(newJobs);
+    setLoading(false);
+  }
   return (
     <div className="App">
      
